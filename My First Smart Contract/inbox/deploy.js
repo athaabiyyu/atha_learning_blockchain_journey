e3308317e71a2824b983df1c7abfe2cb6fc55e26
Aulia Atha <auliaatha123@gmail.com>
2025-07-21 09:56:04 +0700

    Deployment Menggunakan Infura

--- My First Smart Contract/inbox/deploy.js
+++ My First Smart Contract/inbox/deploy.js
@@ -1,5 +1,37 @@
+// mengimpor HDWalletProvider dari Truffle.
 const HDWalletProvider = require('@truffle/hdwallet-provider');
+// mengimpor Web3 dari library web3.
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+// mengambil ABI dan bytecode dari file compile.js.
+const {abi, evm} = require('./compile')
 
-// deploy code will go here
+// membuat provider untuk menghubungkan ke jaringan Sepolia menggunakan HDWalletProvider.
+const provider = new HDWalletProvider(
+  'weasel total culture plug wet thunder hand obtain six student whisper federal',  // mnemonic phrase akun metamaskku
+  'https://sepolia.infura.io/v3/c6b099283cb0442ca4b4dd5784b406aa'                   // Infura Sepolia endpoint
+);
+
+// menghubungkan Web3 ke jaringan Ethereum / blockchain menggunakan provider tadi.
+const web3 = new Web3(provider);
+
+// fungsi untuk melakukan deploy smart contract ke jaringan Ethereum.
+const deploy = async () => {
+       // mendapatkan daftar akun yang tersedia di jaringan.
+       const accounts = await web3.eth.getAccounts();
+       console.log('Deploying from account:', accounts[0]);
+
+       // melakukan deploy smart contract dengan ABI dan bytecode yang sudah dikompilasi.
+       const deployedContract = await new web3.eth.Contract(abi)
+         .deploy({ 
+              data: evm.bytecode.object, 
+              arguments: ['Hello World!'] })
+         .send({ 
+              from: accounts[0], 
+              gas: '1000000', 
+              gasPrice: '20000000000' });
+
+       console.log('Contract deployed to:', deployedContract.options.address);
+       provider.engine.stop(); // menghentikan provider setelah deploy selesai
+}
+
+deploy();
--- My First Smart Contract/inbox/test/Inbox.test.js
+++ My First Smart Contract/inbox/test/Inbox.test.js
@@ -6,23 +6,45 @@ const { abi, evm } = require('../compile');
 
 let accounts;
 let inbox;
+let initialMessage = 'Hello World!';
 
 beforeEach(async () => {
+  // Mengambil daftar akun yang tersedia di jaringan lokal Ganache.
   accounts = await web3.eth.getAccounts();
-
+  
   inbox = await new web3.eth.Contract(abi)
     .deploy({ 
        data: evm.bytecode.object, 
-       arguments: ['Hello World!'] 
-})
+       arguments: [initialMessage] 
+    })
     .send({ 
        from: accounts[0], 
        gas: '1000000', 
-       gasPrice: '20000000000' });
-});
+       gasPrice: '20000000000' 
+    });
+}); 
 
 describe('Inbox Contract', () => {
   it('deploys contract', () => {
-       console.log(inbox);
+       assert.ok(inbox.options.address);
   });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, initialMessage);
+  })
+
+  it('can change the message', async () => {
+    const setMessage = await inbox.methods.setMessage('New Message').send({
+      from : accounts[0], 
+      gas: '1000000', 
+      gasPrice: '20000000000'
+    });
+
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'New Message');
+
+    console.log("Transaction Hash:", setMessage.transactionHash);
+  });
+
 });
